fca74d04429feb6020a915283d7585cf52d39e51
Andrey Dubinin <rakdav@inbox.ru>
2019-05-22 10:25:10 +0200

    five

---
+++ app/src/main/java/com/example/dp/Controller/AgentsAdapter.java
@@ -0,0 +1,41 @@
+package com.example.dp.Controller;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dp.R;
+
+public class AgentsAdapter extends RecyclerView.Adapter<AgentsAdapter.ViewHolder>
+{
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+
+    }
+}
