3c0933e5498ac0021feb4b412643042a778f750b
Eduard Perevalov <edekbplan@gmail.com>
2019-05-25 16:57:48 +0200

    фильтрация работает на карте

--- app/src/main/java/com/example/dp/MapsActivity2.java
+++ app/src/main/java/com/example/dp/MapsActivity2.java
@@ -2,6 +2,8 @@ package com.example.dp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -46,18 +48,152 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         Intent intent = getIntent();
         houses = (ArrayList<House>) getIntent().getSerializableExtra("houss");
 
+
+
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        //комнаты
+        boolean h1=prefs.getBoolean("h1",false);
+        boolean h2=prefs.getBoolean("h2",false);
+        boolean h3=prefs.getBoolean("h3",false);
+        boolean h4=prefs.getBoolean("h4",false);
+        boolean h5=prefs.getBoolean("h5",false);
+        //тип недвижки
+        boolean ofice=prefs.getBoolean("ofiss",false);
+        boolean kvart=prefs.getBoolean("kvart",false);
+        boolean houss=prefs.getBoolean("house",false);
+        boolean land=prefs.getBoolean("ground",false);
+        //тип сделки
+        boolean arenda=prefs.getBoolean("q2",false);
+        boolean prodaza=prefs.getBoolean("q1",false);
+        //город
+        String town = prefs.getString("city",null);
+        //boolean townn = prefs.getBoolean("city",false);
+
+        if(h1==false&&h2==false&&h3==false&&h4==false&&h5==false&&ofice==false&&kvart==false&&houss==false&&land==false&&arenda==false&&prodaza==false&&town==null)
+        {
+            return;
+        }
+        else
+        {
+            //тип недвижимости
+            if(houses.size() !=0) {
+                ArrayList<House> temp = new ArrayList<House>();
+                if (kvart)
+                {
+                    for (House h : houses)
+                    {
+                        if (h.getType() != null && h.getType().equals("квартира")) temp.add(h);
+                    }
+                }
+                if (ofice)
+                {
+                    for (House h : houses) {
+                        if (h.getType() != null && h.getType().equals("офисные помещения")||h.getType().equals("офис")) temp.add(h);
+                    }
+                }
+                if (houss) {
+                    for (House h : houses) {
+                        if (h.getType() != null && h.getType().equals("дом в городской черте")||h.getType().equals("таунхаус")||h.getType().equals("дом")) temp.add(h);
+                    }
+                }
+                if (land) {
+                    for (House h : houses) {
+                        if (h.getType() != null && h.getType().equals("участок в городской черте")||h.getType().equals("участок")) temp.add(h);
+                    }
+                }
+                if(ofice==false&&kvart==false&&houss==false&&land==false)
+                {
+                    temp=houses;
+                }
+///////////////колво комнат
+
+                if(temp.size() !=0) {
+                    ArrayList<House> t1 = new ArrayList<>();
+                    if (h1) {
+                        for (House h : temp) {
+                            if (h.getRooms() != null && h.getRooms().equals("1")) t1.add(h);
+                        }
+                    }
+                    if (h2) {
+                        for (House h : temp) {
+                            if (h.getRooms() != null && h.getRooms().equals("2")) t1.add(h);
+                        }
+                    }
+                    if (h3) {
+                        for (House h : temp) {
+                            if (h.getRooms() != null && h.getRooms().equals("3")) t1.add(h);
+                        }
+                    }
+                    if (h4) {
+                        for (House h : temp) {
+                            if (h.getRooms() != null && h.getRooms().equals("4")) t1.add(h);
+                        }
+                    }
+                    if (h5) {
+                        for (House h : temp) {
+                            if (h.getRooms() != null && h.getRooms().equals("5")) t1.add(h);
+                        }
+                    }
+                    if(h1==false&&h2==false&&h3==false&&h4==false&&h5==false)
+                    {
+                        t1=temp;
+                    }
+                    //////////////тип сделки
+
+                    if (t1.size() !=0){
+                        ArrayList<House> t2 = new ArrayList<>();
+                        if (arenda) {
+                            for (House h : t1) {
+                                if ( h.getDeal()!= null && h.getDeal().equals("аренда")) t2.add(h);
+                            }
+                        }
+                        if (prodaza) {
+                            for (House h : t1) {
+                                if (h.getDeal() != null && h.getDeal().equals("продажа")) t2.add(h);
+                            }
+                        }
+                        if(arenda==false&&prodaza==false)
+                        {
+                            t2=t1;
+                        }
+                        //выбор города
+                        if(t2.size()!=0){
+                            ArrayList<House> t3 = new ArrayList<>();
+                            if (town.contains("Калининград")){
+                                for (House h : t2) {
+                                    if ( h.getCity_title()!= null && h.getCity_title().equals("Калининград")) t3.add(h);
+                                }
+
+                            }
+                            if (town.contains("Светлогорск")){
+                                for (House h : t2) {
+                                    if ( h.getCity_title()!= null && h.getCity_title().equals("Cветлогорск")) t3.add(h);
+                                }
+
+                            }
+                            if (town.contains("Зеленоградск")){
+                                for (House h : t2) {
+                                    if ( h.getCity_title()!= null && h.getCity_title().equals("Зеленоградск")) t3.add(h);
+                                }
+                            }
+                            if(town.contains("false"))
+                            {
+                                t3=t2;
+                            }
+                            houses=t3;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -81,15 +217,10 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             mMap.addMarker(new MarkerOptions().position(posit).title(house.getTitle()));
             i++;
         }
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
 
         if (googleMap != null) {
 
-            // More info: https://developers.google.com/maps/documentation/android/infowindows
             mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
                 @Override
                 public void onInfoWindowClick(Marker marker) {
@@ -110,7 +241,6 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             });
         }
 
-       // mMap.setMinZoomPreference(10);
         LatLng ny = new LatLng(54.7113506, 20.5061358);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ny,10));
 
